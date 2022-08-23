1cdcd362f24981dfd570aadda11c56fb623d2d0f
zevolpato <zevolpato@outlook.com>
2022-08-23 11:50:11 -0300

    first

--- CSharp_Aula1/Form1.Designer.cs
+++ CSharp_Aula1/Form1.Designer.cs
@@ -28,20 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.btnConcatenacao = new System.Windows.Forms.Button();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(106, 54);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(183, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(106, 80);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(183, 20);
+            this.txtSobrenome.TabIndex = 1;
+            // 
+            // btnConcatenacao
+            // 
+            this.btnConcatenacao.Location = new System.Drawing.Point(106, 127);
+            this.btnConcatenacao.Name = "btnConcatenacao";
+            this.btnConcatenacao.Size = new System.Drawing.Size(183, 33);
+            this.btnConcatenacao.TabIndex = 2;
+            this.btnConcatenacao.Text = "Juntar Nomes";
+            this.btnConcatenacao.UseVisualStyleBackColor = true;
+            this.btnConcatenacao.Click += new System.EventHandler(this.btnConcatenacao_Click);
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(106, 189);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(183, 20);
+            this.txtNomeCompleto.TabIndex = 3;
+            this.txtNomeCompleto.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nome Completo";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(402, 54);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(50, 20);
+            this.txtN1.TabIndex = 7;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(402, 84);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(50, 20);
+            this.txtN2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "N1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "N2";
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(402, 127);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(50, 33);
+            this.btnSoma.TabIndex = 11;
+            this.btnSoma.Text = "SOMA";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(402, 189);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(222, 20);
+            this.txtResultado.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Controls.Add(this.btnConcatenacao);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
             this.Name = "Form1";
             this.Text = "Formulário";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.Button btnConcatenacao;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
 
--- CSharp_Aula1/Form1.cs
+++ CSharp_Aula1/Form1.cs
@@ -16,5 +16,50 @@ namespace CSharp_Aula1
         {
             InitializeComponent();
         }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConcatenacao_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string sobrenome = txtSobrenome.Text;
+
+            txtNomeCompleto.Text = nome + " "+ sobrenome; 
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txtN1.Text);
+            int n2 = int.Parse(txtN2.Text);
+
+            int rst = n1 + n2;
+
+            if (rst > 10)
+            {
+                txtResultado.Text = "O valor é : " + rst.ToString() + " que é maior que 10.";
+            }
+            else
+            {
+                if (rst < 10)
+                {
+                    txtResultado.Text = "O valor é : " + rst.ToString() + " que é menor que 10.";
+                }
+                else
+                {
+                    txtResultado.Text = "O valor é : " + rst.ToString() + " igual a 10.";
+                }
+            }
+                
+            
+
+        }
     }
 }
